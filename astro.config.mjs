40f45bc61f1a62a92f43d21d78233eaadabf8ec0
sangsongthong-hexterika <gexterlife.personal@gmail.com>
2024-12-02 05:58:21 +0700

    Add my Bachelor's Degree diplomas, a dd my Mattayom 6 transcript. Add my Skyline HS Transcript, add IT & Cybersecurity Certifications, and add Social  Media Marketing Badge on Main.astro. Add a link to my Credly profile to the Header.astro. Add  attributes to all the external links on Header.astro and Main.astro. Change the name of MySelfiePic.jpg to myprofilepic.jpg and changes all the references there to the correct file name. Created 4 new folders in assets to organize diplomas and certifications.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,8 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import react from '@astrojs/react';
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
@@ -10,4 +12,6 @@ export default defineConfig({
       },
     },
   },
+
+  integrations: [react()],
 });
